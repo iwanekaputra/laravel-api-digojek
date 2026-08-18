b7dfd3538bda4a08b5df7ebb7fec0328fa405276
Your Name <you@example.com>
2026-08-18 18:44:39 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -201,7 +201,7 @@ Route::middleware(LogApiRequest::class)->group(function () {
         Route::get('/v2/drivers/deposit', [DepositDriverController::class, 'index']);
         Route::get('/v2/drivers/deposit/{depositDriver}', [DepositDriverController::class, 'show']);
 
-
+        Route::post('driver/update-status-driver', [DriverController::class, 'updateStatusDriver']);
         // new
         Route::get('/driver/orders/incoming', [DriverOrderController::class, 'incoming']);
         Route::post('/driver/accept-order', [DriverOrderController::class, 'acceptOrder']);
